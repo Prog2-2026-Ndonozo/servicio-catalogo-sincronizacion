604653842aec66e174c10dd136fd02e894018c52
Nehuen <n.donozo@alumno.um.edu.ar>
2026-09-25 17:33:38 -0300

    cfg

---
+++ src/main/java/com/ndonozo/serviciocatalogosincronizacion/ServicioCatalogoSincronizacionApplication.java
@@ -0,0 +1,13 @@
+package com.ndonozo.serviciocatalogosincronizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCatalogoSincronizacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioCatalogoSincronizacionApplication.class, args);
+    }
+
+}
